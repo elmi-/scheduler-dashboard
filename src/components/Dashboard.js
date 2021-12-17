44530e5982cb17686043dc7eb893ef085cdd80c0
Abdi <abdi.elmi@rocketmail.com>
2021-12-17 15:23:16 -0800

    added persistent data using scheduler-api and web sockets to see Dashboard working in realtime

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { setInterview } from "helpers/reducers";
+import axios from "axios";
 
 import classnames from "classnames";
 import Loading from "./Loading";
@@ -9,6 +11,9 @@ class Dashboard extends Component {
   state = {
     loading: false,
     focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {},
   };
 
   selectPanel = id => {
@@ -16,6 +21,49 @@ class Dashboard extends Component {
       focused: previousState.focused !== null ? null : id
     }));
    }
+
+   componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
+
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
+  }
 
   render() {
     const dashboardClasses = classnames("dashboard", {
@@ -25,17 +73,16 @@ class Dashboard extends Component {
     if (this.state.loading) {
       return <Loading />;
     }
-
+    console.log(this.state)
     const panels = data
       .filter(
         panel => this.state.focused === null || this.state.focused === panel.id
       )
       .map(panel => (
         <Panel
-          key={panel.id}
-          id={panel.id}
-          label={panel.label}
-          value={panel.value}
+        key={panel.id}
+        label={panel.label}
+        value={panel.getValue(this.state)}
           onSelect={event => this.selectPanel(panel.id)}
         />
     ));
--- src/helpers/sampleData.js
+++ src/helpers/sampleData.js
@@ -1,23 +1,30 @@
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "./selectors";
+
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
